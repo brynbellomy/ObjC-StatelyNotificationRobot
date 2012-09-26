e70e9528932b98af2e2d6a50433e89eb2850e5f5
bryn austin bellomy <bryn.bellomy@gmail.com>
2012-09-26 10:51:46 -0400

    code documentation by otis + some tinkering with project structure

---
+++ otis.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  inDir: "./Classes",
+  outDir: "../gh-pages",
+  markdownEngine: "showdown",
+  onlyUpdated: false,
+  colourScheme: "friendly",
+  ignoreHidden: true,
+  sidebarState: true,
+  index: "SEStatelyNotificationRobot.h.html",
+  exclude: "*.pch"
+};
